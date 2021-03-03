42fac12a1119432db348ca583a15eafcea07e6eb
unknown <houssemeddine.marsaoui@esprit.tn>
2021-03-03 16:05:03 +0100

    projetvide

---
+++ backEnd/src/main/java/com/esprit/dari/DariApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.dari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DariApplication.class, args);
+    }
+
+}
