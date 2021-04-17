445b340e48a608173199eb1089486ba205b8801e
marsaoui <47007224+marsaoui-houssem@users.noreply.github.com>
2021-04-18 00:38:27 +0100

    Delete FurnituresRepository.java

--- backEnd/src/main/java/com/esprit/dari/dao/FurnituresRepository.java
+++
@@ -1,9 +0,0 @@
-package com.esprit.dari.dao;
-
-import com.esprit.dari.entities.Furnitures;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FurnituresRepository extends CrudRepository<Furnitures, Long> {
-}
