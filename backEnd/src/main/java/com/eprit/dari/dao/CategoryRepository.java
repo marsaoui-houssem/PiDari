da2c60e908093a54a90fda92ea3fc61056e89459
marsaoui <47007224+marsaoui-houssem@users.noreply.github.com>
2021-04-18 00:30:07 +0100

    Delete backEnd/src/main/java/com/eprit/dari directory

--- backEnd/src/main/java/com/eprit/dari/dao/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eprit.dari.dao;
-
-import com.eprit.dari.entities.Category;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Category,Long> {
-}
--- backEnd/src/main/java/com/eprit/dari/dao/CommandRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eprit.dari.dao;
-
-import com.eprit.dari.entities.Command;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CommandRepository extends CrudRepository<Command,Long> {
-}
