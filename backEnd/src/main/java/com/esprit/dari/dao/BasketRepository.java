590ea2c81ab934cc43eab473f0e21a52f484bf7b
marsaoui <47007224+marsaoui-houssem@users.noreply.github.com>
2021-04-18 00:38:15 +0100

    Delete BasketRepository.java

--- backEnd/src/main/java/com/esprit/dari/dao/BasketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.esprit.dari.dao;
-
-import com.esprit.dari.entities.Basket;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BasketRepository extends CrudRepository<Basket,Long> {
-}
